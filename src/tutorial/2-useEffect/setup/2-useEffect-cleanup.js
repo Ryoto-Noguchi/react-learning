40cd54df410faaea156f40109eaaf02e1ce6a34c
Ryoto-Noguchi <ryoto.noguchi@gmail.com>
2021-03-19 07:21:16 +0900

    Full React Course 2020

--- src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
+++ src/tutorial/2-useEffect/setup/2-useEffect-cleanup.js
@@ -1,10 +1,30 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 // cleanup function
 // second argument
 
 const UseEffectCleanup = () => {
-  return <h2>useEffect cleanup</h2>;
+  const [size, setSize] = useState(window.innerWidth);
+
+  const checkSize = () => {
+    setSize(window.innerWidth);
+  };
+  useEffect(() => {
+    console.log("useEffect");
+    console.log("Hello World");
+    window.addEventListener("resize", checkSize); // 画面の幅を変更する度にcheckSizeメソッドを走らせる
+    return () => { // cleanup関数はuseEffectを使用する際には忘れずにつける。そうしないと処理がループしてしまってメモリリークが起きる
+      console.log("cleanup");
+      window.removeEventListener("resize", checkSize);
+    };
+  }, []);
+  console.log("render");
+  return (
+    <>
+      <h1>window</h1>
+      <h2>{size}PX</h2>
+    </>
+  );
 };
 
 export default UseEffectCleanup;
