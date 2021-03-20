2c7e2ad56194666120dd4924af95b0b6f8842232
Ryoto-Noguchi <ryoto.noguchi@gmail.com>
2021-03-20 23:36:09 +0900

    Full React Course 2020 6:02:46

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,12 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // short-circuit evaluation
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+  const [text, setText] = useState("");
+  const firstValue = text || "hello world";
+  const secondValue = text && "hello world";
 
-  return <h2>short circuit</h2>;
+  return (
+    <>
+      {/* <h1>{firstValue}</h1>
+      <h1>value: {secondValue}</h1> */}
+
+      {/* textがfalsyであれば、"john doe"を出力する */}
+      <h1>{text || "john doe"}</h1>
+
+      {/* textがfalsyでなければ「Hello World」を出力する */}
+      {text && <h1>hello world</h1>}
+    </>
+  );
 };
 
 export default ShortCircuit;
